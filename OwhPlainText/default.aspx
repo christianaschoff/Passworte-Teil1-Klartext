78e7d5e883b31ac98c4a1c0a5a40a21dd9ceb69f
christianaschoff <christian.aschoff@diw-sc.de>
2015-08-23 18:46:23 +0200

    Init

---
+++ OwhPlainText/default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="OwhPlainText._default" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>PlainText SQL Injection</title>       
+    <link href="default.css" rel="stylesheet" />
+</head>
+<body>
+
+    <h1>SQL Injection Demo</h1>
+
+    <form id="form1" runat="server">
+        <div>        
+            <div class="bw">Benutzer:</div> <input type="text" id="user" runat="server" />
+            <br /><div class="bw">Passwort:</div> <input type="password" id="pw" runat="server" />
+            <br /><div class="bw">&nbsp;</div>
+            <asp:Button Text="anmelden" runat="server" OnClick="Anmelden_Click" />
+        </div>
+        <br />
+        <br />
+        <br /><strong>Ergebnis:</strong>
+        <div id="erg" runat="server"></div>        
+    </form>
+
+    <br />
+    <br /><strong>SQL Injection:</strong>
+    <br />Im Feld "Benutzer" folgenden Code eingeben:
+    <br /><span class="code">'UNION ALL SELECT * FROM PlainText;--</span>
+    <br />Das Passwort kann beliebig sein.
+</body>
+</html>
---
+++ OwhPlainText/default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OwhPlainText
+{
+    public partial class _default : System.Web.UI.Page
+    {
+
+        protected void Anmelden_Click(object sender, EventArgs e)
+        {
+            var sql = "SELECT * FROM PlainText WHERE Benutzername = '" + user.Value + "' AND Passwort = '" + pw.Value + "';";
+            var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+            using (var comm = new SqlCommand(sql, conn))
+            {
+                conn.Open();
+                ShowResult(comm.ExecuteReader());
+                conn.Close();
+            }            
+        }
+
+        private void ShowResult(SqlDataReader sqlDataReader)
+        {
+            if(!sqlDataReader.HasRows)
+            {
+                erg.InnerHtml = "Benutzer / Passwort nicht gefunden";
+                return;
+            }
+
+            var sb = new StringBuilder();
+            while(sqlDataReader.Read())
+            {
+                sb.AppendLine(string.Format("<div class='bw2'>Benutzer: {0}</div> Passwort: {1}", sqlDataReader.GetString(1), sqlDataReader.GetString(2))); 
+            }
+            erg.InnerHtml = sb.ToString().Replace(Environment.NewLine, "<br />");
+        }
+
+    }
+}
